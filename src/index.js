2775daab242ce4360befc90411633fc1f58d5b3a
danocolombo <danocolombo@gmail.com>
2020-04-19 22:57:58 -0400

    moving metrex sprint05 in

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
-
 ReactDOM.render(
     <div>
-        <App />
+        <h2>Metrex: Health Check</h2>
     </div>,
     document.getElementById("root")
 );
